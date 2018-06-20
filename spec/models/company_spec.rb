ac30c4ab2c3de09d942921b34aa6e82b524a7a7b
kokiBit <koki.takahashi@seattleconsulting.co.jp>
2018-06-21 02:01:25 +0900

    initial rspec

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # 名前があれば有効な状態であること
+  it "is valid with a name" do
+    company = Company.new(
+      name: "Dragon Company"
+    )
+    expect(company).to be_valid
+  end
 end
